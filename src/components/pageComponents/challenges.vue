a0623e3621e76ff68b2d0214045195d63d7effd7
sbboard <colin.buffum@gmail.com>
2021-10-20 22:14:53 -0600

    challenge page

--- src/components/pageComponents/challenges.vue
+++ src/components/pageComponents/challenges.vue
@@ -22,4 +22,15 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+#challenges
+  padding: 0 3em
+  margin-top: 3em
+  height: calc(100vh - 22em)
+  overflow-y: auto
+::v-deep b
+  display: block
+  font-weight: bold
+  margin-top: 1em
+  &:first-of-type
+    margin-top: 0
 </style>
