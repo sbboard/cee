4c9b71e80578c5fd3c6c721dc534275ee4a13ded
sbboard <colin.buffum@gmail.com>
2021-10-28 20:27:05 -0600

    video page

--- src/components/pageComponents/vid.vue
+++ src/components/pageComponents/vid.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="vidPage">
-    <h2>
+    <h2 :class="[`${json[currentIndex].colorScheme}Color`]">
       {{ json[currentIndex].video.title }}
+      <i class="far fa-eye" data-v-ae914fcc=""></i>
     </h2>
-    <div>
-      <img :src="`./img/${json[currentIndex].video.thumbnail}`" />
+    <div id="stuffWrap">
+      <img :src="`/assets/img/${json[currentIndex].video.thumbnail}`" />
       <p>{{ json[currentIndex].video.description }}</p>
     </div>
   </div>
@@ -28,4 +29,40 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+@import "../../../src/global.sass"
+
+h2
+  padding: .5em
+  margin-bottom: 1em
+  position: relative
+  i
+    font-size: 2em
+    position: absolute
+    right: 0.25em
+    top: 0em
+    color: white
+    cursor: pointer
+  &.redColor
+    background-color: rgba($darkRed, .5)
+#stuffWrap
+  width: 100%
+  @include mobile
+    text-align: center
+  img
+    width: 30%
+    float: left
+    box-shadow: 3px 4px 6px 1px rgba(0, 0, 0, .5)
+    @include mobile
+      max-height: 50vh
+      max-width: 100%
+      width: auto
+      margin: 0 auto
+      margin-bottom: 1em
+      float: none
+  p
+    display: block
+    margin-left: calc(30% + 1em)
+    text-align: left
+    @include mobile
+      margin-left: 0
 </style>
